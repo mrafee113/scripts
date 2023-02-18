245e0154ab7e95fe03f26f89b7b6c8aaa097c81f
Mehdi Rafee <mrafee113@gmail.com>
2023-02-18 19:08:17 +0330

    Added link-fetcher.py

---
+++ link-fetcher.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+
+from lxml import html
+from urllib.parse import urlsplit
+
+if len(sys.argv) < 2:
+    exit(1)
+
+url = sys.argv[1]
+s = urlsplit(url)
+r = requests.get(url)
+tree = html.document_fromstring(r.text)
+viable_elements = tree.xpath('//body//a[@href]')
+links = list()
+for el in viable_elements:
+    try:
+        links.append(s.scheme + '://' + s.hostname + s.path + str(el.attrib['href']))
+    except Exception:
+        pass
+
+with open("/tmp/links.txt", 'w') as file:
+    file.write('\n'.join(links))
+
+exit(1)
